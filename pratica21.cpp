dbbcb9f662db9f1017cc96cbf5d336674f78391d
Luiz Felipe <luizfelipecrx@gmail.com>
2023-10-13 23:45:29 -0300

    Implementa listar livros

--- pratica21.cpp
+++ pratica21.cpp
@@ -6,76 +6,110 @@
 
 using namespace std;
 
-class Livro {
-    private:
-        string titulo;
-        string autor;
-        int numCopias;
-    public:
-        Livro(const string& titulo, const string& autor, int numCopias) :
-            titulo(titulo), autor(autor), numCopias(numCopias) {
-        }
+class Livro
+{
+private:
+    string titulo;
+    string autor;
+    int numCopias;
 
-        const string& getTitulo() const {
-            return titulo;
-        }
+public:
+    Livro(const string &titulo, const string &autor, int numCopias) : titulo(titulo), autor(autor), numCopias(numCopias)
+    {
+    }
 
-        const string& getAutor() const {
-            return autor;
-        }
+    const string &getTitulo() const
+    {
+        return titulo;
+    }
 
-        int getNumCopias() const {
-            return numCopias;
-        }
+    const string &getAutor() const
+    {
+        return autor;
+    }
 
-        void emprestar() {
-            if (numCopias > 0) {
-                numCopias--;
-            }
-        }
+    int getNumCopias() const
+    {
+        return numCopias;
+    }
 
-        void devolver() {
-            numCopias++;
+    void emprestar()
+    {
+        if (numCopias > 0)
+        {
+            numCopias--;
         }
+    }
+
+    void devolver()
+    {
+        numCopias++;
+    }
 };
 
-class Usuario {
-    private:
-        string nome;
-        vector<Livro*> livrosEmprestados;
-        vector<time_t> tempoEmprestimo;
-    public:
-        Usuario(const string& nome) : nome(nome) {
-        }
+class Usuario
+{
+private:
+    string nome;
+    vector<Livro *> livrosEmprestados;
+    vector<time_t> tempoEmprestimo;
 
-        const string& getNome() const {
-            return nome;
+public:
+    Usuario(const string &nome) : nome(nome)
+    {
+    }
+
+    const string &getNome() const
+    {
+        return nome;
+    }
+    void emprestarLivro(Livro &livro)
+    {
+        if (livro.getNumCopias() > 0)
+        {
+            livro.emprestar();
+            livrosEmprestados.push_back(&livro);
+            tempoEmprestimo.push_back(time(nullptr));
         }
-        void emprestarLivro(Livro& livro) {
-            if (livro.getNumCopias() > 0) {
-                livro.emprestar();
-                livrosEmprestados.push_back(&livro);
-                tempoEmprestimo.push_back(time(nullptr));
+    }
+
+    void devolverLivro(Livro &livro)
+    {
+
+        int pos = livrosEmprestados.size();
+        for (int i = 0; i < pos; i++)
+        {
+            if (livrosEmprestados[i] == &livro)
+            {
+                livro.devolver();
+                livrosEmprestados.erase(livrosEmprestados.begin() + i);
+                tempoEmprestimo.erase(tempoEmprestimo.begin() + i);
             }
         }
+    }
 
-        void devolverLivro(Livro& livro){
-            
-            int pos = livrosEmprestados.size();
-            for(int i=0;i<pos;i++){
-                if( livrosEmprestados[i]== &livro ){
-                    livro.devolver();
-                    livrosEmprestados.erase(livrosEmprestados.begin()+i);
-                    tempoEmprestimo.erase(tempoEmprestimo.begin()+i);
-                }
+    void listarLivrosEmprestados()
+    {
+        if (livrosEmprestados.empty())
+        {
+            cout << "O usuário não tem livros emprestados." << endl;
+        }
+        else
+        {
+            cout << "Livros emprestados por " << nome << ":" << endl;
+            for (size_t i = 0; i < livrosEmprestados.size(); i++)
+            {
+                cout << "Título: " << livrosEmprestados[i]->getTitulo() << endl;
+                cout << "Autor: " << livrosEmprestados[i]->getAutor() << endl;
+                cout << "Dias emprestado: " << tempoEmprestimo[i] << " dias" << endl;
+                cout << "---------------------" << endl;
             }
         }
-
-        // listar livro emprestado
-
+    }
 };
 
-int main() {
+int main()
+{
 
     // Criando livros
     Livro livro1("As Viagens de Gulliver", "Jonathan Swift", 5);
@@ -96,7 +130,6 @@ int main() {
     usuario2.listarLivrosEmprestados();
 
     usuario1.devolverLivro(livro1);
-    
 
     return 0;
 }
